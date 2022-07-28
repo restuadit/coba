1fa6fa5bacb17eac3140b16dabdee46cbdcb5204
restuadit <101609333+restuadit@users.noreply.github.com>
2022-07-28 14:17:20 +0700

    Update login.php

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ session_start()
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN</title>
-    <link rel="stylesheet" href="bootstrap/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -68,6 +68,6 @@ if (isset($_POST['login'])) {
       </div>
   </div>
   </div>
-  <script src="bootstrap/bootstrap/js/bootstrap.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
